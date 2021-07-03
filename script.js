3400eb263707c4d177a438465b4c1661eeeb0eed
Devang Manjramkar <devangmanjramkar@gmail.com>
2021-07-03 23:06:50 +0530

    new

--- script.js
+++ script.js
@@ -4,51 +4,42 @@ const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
 const progressBarPercents = [97, 92, 20, 98, 98, 92, 98, 0];
-
 window.addEventListener("scroll", () => {
-  mainFn();
+	mainFn();
 });
-
 const mainFn = () => {
-  if (window.pageYOffset >= navbarOffsetTop) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
+	if (window.pageYOffset >= navbarOffsetTop) {
+		navbar.classList.add("sticky");
+	} else {
+		navbar.classList.remove("sticky");
+	}
 
-function myFunction() {
-  var x = document.getElementById("navbar-id");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
-
-  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
-    document.querySelectorAll(".progress-percent").forEach((el, i) => {
-      el.style.width = `${progressBarPercents[i]}%`;
-      el.previousElementSibling.firstElementChild.textContent =
-        progressBarPercents[i];
-    });
-  }
+	function myFunction() {
+		var x = document.getElementById("navbar-id");
+		if (x.className === "navbar") {
+			x.className += " responsive";
+		} else {
+			x.className = "navbar";
+		}
+	}
+	if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+		document.querySelectorAll(".progress-percent").forEach((el, i) => {
+			el.style.width = `${progressBarPercents[i]}%`;
+			el.previousElementSibling.firstElementChild.textContent = progressBarPercents[i];
+		});
+	}
 };
-
-(function() {
-
-  var img = document.getElementById('zoom').firstChild;
-  img.onload = function() {
-      if(img.height > img.width) {
-          img.height = '100%';
-          img.width = 'auto';
-      }
-  };
-  
-  }());
-
+(function () {
+	var img = document.getElementById('zoom').firstChild;
+	img.onload = function () {
+		if (img.height > img.width) {
+			img.height = '100%';
+			img.width = 'auto';
+		}
+	};
+}());
 mainFn();
-
 window.addEventListener("resize", () => {
-  window.location.reload();
+	window.location.reload();
 });
 
